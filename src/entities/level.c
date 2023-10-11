664cf1b9bb74a8f9441ee0b2c0891a60501beb83
Junior Farias <fariasjunior96@gmail.com>
2023-10-11 13:09:40 -0300

    Streamlined level loading code

--- src/entities/level.c
+++ src/entities/level.c
@@ -10,58 +10,6 @@
 #include "../assets.h"
 
 
-typedef struct LevelEnemy {
-    Vector2 pos;
-} LevelEnemy;
-
-typedef struct LevelBlock {
-    Rectangle rect;
-} LevelBlock;
-
-typedef struct LevelData {
-
-    int enemyCount;
-    LevelEnemy *enemies;
-
-    int blockCount;
-    LevelBlock *blocks;
-
-} LevelData;
-
-
-Texture2D floorTileTexture;
-
-
-LevelData loadLevelData() {
-
-    LevelData data;
-    data.enemyCount = 1;
-    data.blockCount = 6;
-
-    data.enemies = MemAlloc(sizeof(LevelEnemy) * data.enemyCount);
-    data.blocks = MemAlloc(sizeof(LevelBlock) * data.blockCount);
-
-    data.blocks[0].rect = (Rectangle){ 0,                           FLOOR_HEIGHT,       BlockSprite.sprite.width*25, BlockSprite.sprite.height*5 };
-
-    data.blocks[1].rect = (Rectangle){ BlockSprite.sprite.width*30,  FLOOR_HEIGHT,       BlockSprite.sprite.width*10, BlockSprite.sprite.height*5 };
-    data.enemies[0].pos = (Vector2){ data.blocks[1].rect.x + (data.blocks[1].rect.width / 2),
-                                        200 };
-
-    data.blocks[2].rect = (Rectangle){ BlockSprite.sprite.width*45,  FLOOR_HEIGHT-80,    BlockSprite.sprite.width*10, BlockSprite.sprite.height*2 };
-
-    data.blocks[3].rect = (Rectangle){ BlockSprite.sprite.width*40,  FLOOR_HEIGHT-200,   BlockSprite.sprite.width*5,  BlockSprite.sprite.height*1 };
-
-    data.blocks[4].rect = (Rectangle){ BlockSprite.sprite.width*45,  FLOOR_HEIGHT-320,   BlockSprite.sprite.width*5,  BlockSprite.sprite.height*1 };
-
-    return data;
-}
-
-void freeLoadedLevelData(LevelData data) {
-
-    MemFree(data.blocks);
-    MemFree(data.enemies);
-} 
-
 /*
     Snaps a coordinate (x or y) into the grid BlockSprites.
 */
@@ -69,38 +17,33 @@ float snapToGrid(float v) {
     return v - (abs(v) % (int) BlockSprite.sprite.width); // BlockSprite is square, so same for x and y.
 }
 
-Entity *addBlockToLevel(Entity *head, LevelBlock block) {
+Entity *addBlockToLevel(Entity *head, Rectangle hitbox) {
     Entity *entity = MemAlloc(sizeof(Entity));
 
-    block.rect.x = snapToGrid(block.rect.x);
-    block.rect.y = snapToGrid(block.rect.y);
+    hitbox.x = snapToGrid(hitbox.x);
+    hitbox.y = snapToGrid(hitbox.y);
 
     entity->components = HasPosition +
                             HasSprite +
                             IsLevelElement;
-    entity->hitbox = block.rect;
+    entity->hitbox = hitbox;
     entity->sprite = BlockSprite;
 
     return AddToEntityList(head, entity);
 }
 
 Entity *InitializeLevel(Entity *head) {
+
+    head = addBlockToLevel(head, (Rectangle){ 0, FLOOR_HEIGHT, BlockSprite.sprite.width*25, BlockSprite.sprite.height*5 });
     
-    floorTileTexture = LoadTexture("../assets/floor_tile_1.png");
-
-    LevelData data = loadLevelData();
-
-    LevelBlock *block = data.blocks;
-    for (int idx = 0; idx < data.blockCount; idx++) {
-        head = addBlockToLevel(head, block[idx]);
-    }
-
-    LevelEnemy *enemy = data.enemies;
-    for (int idx = 0; idx < data.enemyCount; idx++) {
-        head = InitializeEnemy(head, enemy[idx].pos.x, enemy[idx].pos.y);
-    }
-
-    freeLoadedLevelData(data);
+    float x = BlockSprite.sprite.width*30;
+    float width = BlockSprite.sprite.width*10;
+    head = addBlockToLevel(head, (Rectangle){ x, FLOOR_HEIGHT, width, BlockSprite.sprite.height*5 });
+    head = InitializeEnemy(head, x + (width / 2), 200);
+    
+    head = addBlockToLevel(head, (Rectangle){ BlockSprite.sprite.width*45, FLOOR_HEIGHT-80, BlockSprite.sprite.width*10, BlockSprite.sprite.height*2 });
+    head = addBlockToLevel(head, (Rectangle){ BlockSprite.sprite.width*40, FLOOR_HEIGHT-200, BlockSprite.sprite.width*5, BlockSprite.sprite.height*1 });
+    head = addBlockToLevel(head, (Rectangle){ BlockSprite.sprite.width*45, FLOOR_HEIGHT-320, BlockSprite.sprite.width*5, BlockSprite.sprite.height*1 });
 
     SetEntityPosition(PLAYER, SCREEN_WIDTH/5, 300);
 
@@ -125,9 +68,5 @@ void AddBlockToLevel(Entity *head, Vector2 pos) {
 
     }
 
-    LevelBlock block = {
-        { pos.x, pos.y, BlockSprite.sprite.width, BlockSprite.sprite.height }
-    };
-
-    addBlockToLevel(head, block);
+    addBlockToLevel(head, (Rectangle){ pos.x, pos.y, BlockSprite.sprite.width, BlockSprite.sprite.height });
 }
