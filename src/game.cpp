ee5d81414674cbb479f0a1433a715509f0db7d50
Junior Farias <fariasjunior96@gmail.com>
2024-05-05 13:08:38 -0300

    Fixed window on Windows before toggle

--- src/game.cpp
+++ src/game.cpp
@@ -5,12 +5,33 @@
 #include "input.hpp"
 #include "overworld.hpp"
 
+void initWindow() {
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Jogo de plataforma");
+
+    /*
+        Windows for some reason tries to resize the window to slightly lower than 1080p (vertical)
+        in my 1080p monitor in windowed mode.
+
+        After entering and then exiting fullscreen we just have a image that's cropped in the
+        bottom, which is ok and fixable with only SetWindowMinSize(), if desired.
+        
+        But when the game first launches in windowed mode, for some reason,
+        the rendering is dislocated upwards, putting it off sync with the game internal logic and
+        breaking the mouse interactions.
+
+        This combination of commands fixes this. 
+    */
+    SetWindowMinSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    SetWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
 int main() {
 
     SetTraceLogLevel(LOG_DEBUG);
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Jogo de plataforma");
-    
+    initWindow();
+
     SetTargetFPS(60);
 
     SystemsInitialize();
