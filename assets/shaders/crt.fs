14f9c47fff22f7bb4fa5a596cc50b919e6b7d387
Junior Farias <fariasjunior96@gmail.com>
2024-06-12 21:32:16 -0300

    Added Big RBG effect to the CRT Shader

--- assets/shaders/crt.fs
+++ assets/shaders/crt.fs
@@ -10,6 +10,9 @@ uniform sampler2D texture0; // underlying texture, automatically provided
 uniform vec2 u_resolution;  // resolution of the scene
 
 
+/*
+	Applies CRT distortion to the screen.
+*/
 vec2 crt_distort(vec2 uv) {
 	
 	uv = uv * 2.0 - 1.0;
@@ -24,9 +27,39 @@ vec2 crt_distort(vec2 uv) {
 	return uv;
 }
 
+/*
+	Greys out every other line to simulate scanlines.
+*/
 vec3 scanlines(vec3 pixelColors, float pixelY) {
 
 	return pixelColors *= 1.0 - mod(pixelY, 2.0) * 0.1;
+}
+
+/*
+	Shift the colors of the pixel so it looks like the screen
+	is made of big RGB pixels.
+*/
+vec3 big_rgb(vec3 pixelColors, float pixelX) {
+
+	switch(int(mod(pixelX, 3.0))) {
+		case 0:
+			pixelColors.r *= 1.1;
+			pixelColors.g *= 0.8;
+			pixelColors.b *= 0.8;
+			break;
+		case 1:
+			pixelColors.r *= 0.8;
+			pixelColors.g *= 1.1;
+			pixelColors.b *= 0.8;
+			break;
+		case 2:
+			pixelColors.r *= 0.8;
+			pixelColors.g *= 0.8;
+			pixelColors.b *= 1.1;
+			break;		
+	}
+
+	return pixelColors;
 }
 
 void main() {
@@ -41,6 +74,10 @@ void main() {
 
 	colors = texture(texture0, uv).rgb;
 	
+
+	// Apply Big RBG effect
+	colors = big_rgb(colors, gl_FragCoord.x);
+
 
 	// Apply scanlines
 	colors = scanlines(colors, gl_FragCoord.y);
